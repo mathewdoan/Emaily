d5617dd65de7f07dc38c0b312e6012fee7dd665e
Mathew Doan <mathewdoan@Mathews-MacBook-Pro.local>
2018-01-31 19:55:34 -0800

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send({ hello: "how are you" });
+	res.send({ bye: "bitch" });
 });
 const PORT =  process.env.PORT || 5000;
 app.listen(PORT);
