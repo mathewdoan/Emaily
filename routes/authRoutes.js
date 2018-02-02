691519a89d9037fd85d5e2ba966939bb6e87c70e
Mathew Doan <mathewdoan@Mathews-MacBook-Pro.local>
2018-02-02 13:16:28 -0800

    fixed routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -25,15 +25,15 @@ app.get("/api/current_user", (req, res) => {
 
 
 
-app.get("/auth/facebook", passport.authenticate("facebook"));
-
-app.get(
-	"/auth/facebook/callback",
-	passport.authenticate("facebook", { failureRedirect: "/login" }),
-	function(req, res) {
+//app.get("/auth/facebook", passport.authenticate("facebook"));
+//
+//app.get(
+//	"/auth/facebook/callback",
+//	passport.authenticate("facebook", { failureRedirect: "/login" }),
+//	function(req, res) {
 		// Successful authentication, redirect home.
-		res.send(req.user);
-	}
-);
+//		res.send(req.user);
+//	}
+//);
 
 };
