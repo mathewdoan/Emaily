025ebdd5a939d294883b85f95835f525cd566af3
Mathew Doan <mathewdoan@Mathews-MacBook-Pro.local>
2018-02-02 15:57:24 -0800

    again

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -24,16 +24,4 @@ app.get("/api/current_user", (req, res) => {
 
 
 
-
-app.get("/auth/facebook", passport.authenticate("facebook"));
-
-app.get(
-	"/auth/facebook/callback",
-	passport.authenticate("facebook", { failureRedirect: "/login" }),
-	function(req, res) {
-		// Successful authentication, redirect home.
-		res.send(req.user);
-	}
-);
-
 };
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,5 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
@@ -34,27 +33,6 @@ passport.use(
 					new User({ googleId: profile.id })
 						.save()
 						.then(user => done(null, user));
-				}
-			});
-		}
-	)
-);
-
-passport.use(
-	new FacebookStrategy(
-		{
-			clientID: keys.facebookClientID,
-			clientSecret: keys.facebookClientSecret,
-			callbackURL: "http://localhost:5000/auth/facebook/callback"
-		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ facebookId: profile.id }).then(existingUser => {
-				if (existingUser) {
-				done(null, existingUser);
-			} else {
-				new User ({ facebookId: profile.id })
-					.save()
-					.than(user => done(null, user))
 				}
 			});
 		}
