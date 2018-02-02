449f7cda57a933ed7cd272894dd31380c8ffe34b
Mathew Doan <mathewdoan@Mathews-MacBook-Pro.local>
2018-02-02 12:33:01 -0800

    Completed auth

---
+++ services/passport.js
@@ -0,0 +1,65 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback"
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					//we already have a record w the given profile ID.
+					done(null, existingUser);
+				} else {
+					//we dont have a record w this ID. make a record
+					new User({ googleId: profile.id })
+						.save()
+						.then(user => done(null, user));
+				}
+			});
+		}
+	)
+);
+
+passport.use(
+	new FacebookStrategy(
+		{
+			clientID: keys.facebookClientID,
+			clientSecret: keys.facebookClientSecret,
+			callbackURL: "http://localhost:5000/auth/facebook/callback"
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ facebookId: profile.id }).then(existingUser => {
+				if (existingUser) {
+				done(null, existingUser);
+			} else {
+				new User ({ facebookId: profile.id })
+					.save()
+					.than(user => done(null, user))
+				}
+			});
+		}
+	)
+);
+
+
+
+
