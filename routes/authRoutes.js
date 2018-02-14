22066d6c8b8fa6bd690d03b93dbd0075b98aee57
Mathew Doan <mathewdoan@Mathews-MacBook-Pro.local>
2018-02-13 18:27:57 -0800

    added billing and client side app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,6 @@
 const passport = require("passport");
 
-
-module.exports = (app) => {
+module.exports = app => {
 	app.get(
 		"/auth/google",
 		passport.authenticate("google", {
@@ -9,19 +8,20 @@ module.exports = (app) => {
 		})
 	);
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+	app.get("/auth/google/callback", 
+		passport.authenticate("google"),
+		(req, res) => {
+				res.redirect('/surveys');
+			}
+		);
 
-app.get("/api/logout", (req, res) => {
-	req.logout();
-	res.send(req.user);
-});
 
-
-app.get("/api/current_user", (req, res) => {
-	res.send(req.user);
+	app.get("/api/logout", (req, res) => {
+		req.logout();
+		res.redirect('/');
 	});
 
-
-
-
+	app.get("/api/current_user", (req, res) => {
+		res.send(req.user);
+	});
 };
